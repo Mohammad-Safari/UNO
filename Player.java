9f8f9dfeb1e9291f7fd199e0a0c2f4e66b076cdb
Mohammad Safari <msafari8051@gmail.com>
2020-04-13 17:09:26 +0430

    player class

--- Player.java
+++ Player.java
@@ -1,4 +1,19 @@
+import java.util.*;
 
 public class Player {
-    
+    private List<Card> deck;
+    private int score;
+
+    public Player(List<Card> deck) {
+        this.deck = deck;
+    }
+
+    public boolean putCard(Deck gDeck, int index) {
+
+        return false;
+    }
+
+    public void drawCard(Card card) {
+        deck.add(card);
+    }
 }
